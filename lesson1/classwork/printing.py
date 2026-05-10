23c3f562ab2f2c5f225ca56cf7dd1dff7ff84768
Rey123598 <caveminervillager@gmail.com>
2026-05-10 01:01:16 +0000

    Finish Lesson 1

---
+++ lesson1/classwork/printing.py
@@ -0,0 +1,4 @@
+print("Hello World!")
+print("My name is reyaash!")
+print('My favorite food is sushi!')
+# ("noo")
---
+++ lesson1/classwork/variables.py
@@ -0,0 +1,12 @@
+x = 5 # integer: whole number
+print(x)
+
+number = 3.9812
+print(number)
+P = 4
+print(P)
+fav = "sushi"
+print ("I love eating",fav)
+
+
+
